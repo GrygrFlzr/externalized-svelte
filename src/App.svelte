0e2f2ee01077c1f9c437c69b6f6634508f9ed160
GrygrFlzr <git@cybeast.dev>
2024-02-29 01:56:50 +0900

    Static list

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,52 @@
-<h1>Todo App</h1>
+<script>
+  let valid = $state(true);
+  let invalid = $derived(!valid);
+</script>
+
+<section aria-labelledby="todos-label">
+  <h1 id="todos-label">stuff to do</h1>
+  <ul>
+    <li>Do laundry</li>
+    <li>Cook dinner</li>
+    <li>Find a bird</li>
+  </ul>
+  <div class="empty-state">
+    <p>
+      Either you've done everything on the list or there are still things to add
+      to your list.
+    </p>
+    <p>Add your first todo!</p>
+  </div>
+  <form>
+    <label for="add-todo" class="visually-hidden">Add a todo item</label>
+    <input
+      id="add-todo"
+      type="text"
+      placeholder="E.g. adopt an owl"
+      aria-invalid={invalid}
+    />
+    <button type="submit" disabled={invalid}>Add</button>
+  </form>
+</section>
+
+<style>
+  .visually-hidden {
+    position: absolute !important;
+    clip: rect(1px, 1px, 1px, 1px) !important;
+    padding: 0 !important;
+    border: 0 !important;
+    height: 1px !important;
+    width: 1px !important;
+    overflow: hidden !important;
+  }
+  .empty-state {
+    display: none;
+  }
+  ul:empty + .empty-state {
+    display: block;
+  }
+  #add-todo::placeholder {
+    /** TODO: set color of placeholder text */
+    font-style: italic;
+  }
+</style>
